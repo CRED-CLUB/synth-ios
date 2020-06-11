e433c9357f80109e940a05738d34232ed7bda4ed
Prashant Shrivastava <prashant.shrivastava@cred.club>
2020-06-11 22:29:58 +0530

    add neumorphic kit xcodeProj

---
+++ NeumorphicKit/NeumorphicKit.h
@@ -0,0 +1,19 @@
+//
+//  NeumorphicKit.h
+//  NeumorphicKit
+//
+//  Created by Prashant Shrivastava on 11/06/20.
+//  Copyright © 2020 CRED. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NeumorphicKit.
+FOUNDATION_EXPORT double NeumorphicKitVersionNumber;
+
+//! Project version string for NeumorphicKit.
+FOUNDATION_EXPORT const unsigned char NeumorphicKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NeumorphicKit/PublicHeader.h>
+
+
