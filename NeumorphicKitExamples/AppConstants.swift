3644a659f95fd336b96f6b4d54fdde72123cb11e
Prashant Shrivastava <prashant.shrivastava@cred.club>
2020-10-24 14:35:59 +0530

    add elevated soft button

--- NeumorphicKitExamples/AppConstants.swift
+++ NeumorphicKitExamples/AppConstants.swift
@@ -11,4 +11,17 @@ import UIKit
 struct AppConstants {
     
     static let baseColor = UIColor(red: 33.0/255.0, green: 36.0/255.0, blue: 38.0/255.0, alpha: 1.0)
+    static let textColor = UIColor(red: 249.0/255.0, green: 211.0/255.0, blue: 180.0/255.0, alpha: 1.0)
+    
+    static func getBorderedAttributedText(with text: String) -> NSAttributedString {
+        
+        let strokeColor = UIColor(red: 249.0/255.0, green: 211.0/255.0, blue: 180.0/255.0, alpha: 1.0)
+        let textAttributes: [NSAttributedString.Key:Any] = [
+            .strokeColor: strokeColor,
+            .foregroundColor: UIColor.clear,
+            .strokeWidth: -2.0,
+            .font: UIFont.boldSystemFont(ofSize: 50)
+        ]
+        return NSAttributedString(string: text, attributes: textAttributes)
+    }
 }
--- NeumorphicKitExamples/ViewControllers/ButtonsViewController.swift
+++ NeumorphicKitExamples/ViewControllers/ButtonsViewController.swift
@@ -10,13 +10,72 @@ import UIKit
 
 class ButtonsViewController: UIViewController {
     
-    @IBAction func backButtonClicked(_ sender: UIButton) {
-        self.navigationController?.popViewController(animated: true)
-    }
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var lineView: UIView!
+    @IBOutlet weak var dotView: UIView!
+    @IBOutlet weak var elevatedSoftLabel: UILabel!
+    @IBOutlet weak var elevatedSoftView: UIView!
+    @IBOutlet weak var elevatedSoftWithImageButton: UIButton!
+    @IBOutlet weak var elevatedSoftButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupViews()
+    }
+    
+    private func setupViews() {
         
         view.backgroundColor = AppConstants.baseColor
+        
+        titleLabel.attributedText = AppConstants.getBorderedAttributedText(with: "buttons")
+        lineView.backgroundColor = UIColor(red: 178.0/255.0, green: 134.0/255.0, blue: 99.0/255.0, alpha: 0.2)
+        
+        setupBackButton()
+        setupDotView()
+        updateView(view: elevatedSoftView, label: elevatedSoftLabel, with: "ELEVATED SOFT")
+        
+        let textAttributes: [NSAttributedString.Key:Any] = [
+            .foregroundColor: AppConstants.textColor.withAlphaComponent(0.9),
+            .kern: 0.65,
+            .font: UIFont.systemFont(ofSize: 14, weight: .semibold)
+        ]
+        elevatedSoftWithImageButton.applyNeuBtnStyle(type: .elevatedSoft, attributedTitle: NSAttributedString(string: "Idle now", attributes: textAttributes), image: UIImage(named: "plus"), imageDimension: 10)
+        elevatedSoftButton.applyNeuBtnStyle(type: .elevatedSoft, attributedTitle: NSAttributedString(string: "Idle now", attributes: textAttributes))
+    }
+    
+    private func setupBackButton() {
+        
+        backButton.backgroundColor = UIColor(red: 81.0/255.0, green: 79.0/255.0, blue: 79.0/255.0, alpha: 0.23)
+        backButton.layer.cornerRadius = backButton.bounds.height/2
+        backButton.addTarget(self, action: #selector(backButtonClicked), for: .touchUpInside)
+    }
+    
+    private func setupDotView() {
+        
+        let gradient: CAGradientLayer = CAGradientLayer()
+        gradient.colors = [UIColor(red: 251.0/255.0, green: 195.0/255.0, blue: 150.0/255.0, alpha: 1.0).cgColor, UIColor(red: 99.0/255.0, green: 69.0/255.0, blue: 44.0/255.0, alpha: 1.0).cgColor]
+        gradient.startPoint = .zero
+        gradient.endPoint = CGPoint(x: 1.0, y: 1.0)
+        gradient.frame = dotView.bounds
+
+        dotView.layer.insertSublayer(gradient, at: 0)
+        dotView.layer.cornerRadius = dotView.bounds.height/2
+        dotView.layer.masksToBounds = true
+    }
+    
+    private func updateView(view: UIView, label: UILabel, with text: String) {
+        
+        let textAttributes: [NSAttributedString.Key:Any] = [
+            .foregroundColor: UIColor.white.withAlphaComponent(0.5),
+            .kern: 4,
+            .font: UIFont.systemFont(ofSize: 10, weight: .medium)
+        ]
+        label.attributedText = NSAttributedString(string: text, attributes: textAttributes)
+        view.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+    }
+    
+    @objc private func backButtonClicked() {
+        self.navigationController?.popViewController(animated: true)
     }
 }
