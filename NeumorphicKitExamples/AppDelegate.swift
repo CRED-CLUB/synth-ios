c31b16b546d6b7d2be085f01f993e8971f5e0cce
Prashant Shrivastava <prashant.shrivastava@cred.club>
2020-10-24 12:44:04 +0530

    set root view controller manually

--- NeumorphicKitExamples/AppDelegate.swift
+++ NeumorphicKitExamples/AppDelegate.swift
@@ -12,5 +12,20 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return setRootViewController()
+    }
+        
+    private func setRootViewController() -> Bool {
+        
+        guard let rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ViewController") as? ViewController else {
+            return false
+        }
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+        self.window?.rootViewController = rootViewController
+        self.window?.makeKey()
+        return true
+    }
 }
 
