79dc402f45c7634562b657c95d87ebcce6beed0d
Prashant Shrivastava <prashant.shrivastava@cred.club>
2020-12-18 03:05:37 +0530

    add extension to get UIColor from hex code

--- NeumorphicKitExamples/AppConstants.swift
+++ NeumorphicKitExamples/AppConstants.swift
@@ -25,3 +25,31 @@ struct AppConstants {
         return NSAttributedString(string: text, attributes: textAttributes)
     }
 }
+
+extension UIColor {
+    
+    static func fromHex(_ hex: String, alpha opacity: CGFloat? = nil) -> UIColor {
+        let colorCode = hex.suffix(6)
+        let hexint = Int(intFromHexString(hexStr: String(colorCode)))
+        let red: CGFloat = CGFloat((hexint & 0xff0000) >> 16) / 255.0
+        let green: CGFloat = CGFloat((hexint & 0xff00) >> 8) / 255.0
+        let blue: CGFloat = CGFloat((hexint & 0xff) >> 0) / 255.0
+        let alpha: CGFloat
+        if hex.count >= 8 {
+            let alphaCode = hex.suffix(8).prefix(2)
+            let alphaHexInt = Int(intFromHexString(hexStr: String(alphaCode)))
+            alpha = opacity ?? CGFloat(alphaHexInt & 0xff) / 255.0
+        } else {
+            alpha = opacity ?? 1.0
+        }
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+
+    private static func intFromHexString(hexStr: String) -> UInt32 {
+        var hexInt: UInt32 = 0
+        let scanner: Scanner = Scanner(string: hexStr)
+        scanner.charactersToBeSkipped = CharacterSet(charactersIn: "#")
+        scanner.scanHexInt32(&hexInt)
+        return hexInt
+    }
+}
