2a734a0db71c7dfce0ea6f7656c78ef61261d8d8
Prashant Shrivastava <prashant.shrivastava@cred.club>
2021-01-10 02:18:58 +0530

    add fiber assets

--- SynthExamples/ViewControllers/ViewController.swift
+++ SynthExamples/ViewControllers/ViewController.swift
@@ -11,6 +11,8 @@ import Synth
 class ViewController: UIViewController {
 
     @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var buttonsImageView: UIImageView!
+    @IBOutlet weak var gutterImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,14 +23,20 @@ class ViewController: UIViewController {
         
         containerView.layer.cornerRadius = 20
         containerView.applyNeuStyle()
+        
+        let buttonTapGesture = UITapGestureRecognizer(target: self, action: #selector(exploreButtonsClicked))
+        buttonsImageView.addGestureRecognizer(buttonTapGesture)
+        
+        let gutterTapGesture = UITapGestureRecognizer(target: self, action: #selector(exploreGutterClicked))
+        gutterImageView.addGestureRecognizer(gutterTapGesture)
     }
     
-    @IBAction func exploreButtonsClicked(_ sender: UIButton) {
+    @objc func exploreButtonsClicked() {
         let buttonsVC = ButtonsViewController(nibName: "ButtonsViewController", bundle: nil)
         navigationController?.pushViewController(buttonsVC, animated: true)
     }
     
-    @IBAction func exploreGutterClicked(_ sender: UIButton) {
+    @objc func exploreGutterClicked() {
         let gutterVC = GutterViewController(nibName: "GutterViewController", bundle: nil)
         navigationController?.pushViewController(gutterVC, animated: true)
     }
