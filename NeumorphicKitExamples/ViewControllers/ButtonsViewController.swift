436d2725871183552381e754664ad680067dab07
Prashant Shrivastava <prashant.shrivastava@cred.club>
2021-01-09 15:06:54 +0530

    add license to view controllers

--- NeumorphicKitExamples/ViewControllers/ButtonsViewController.swift
+++ NeumorphicKitExamples/ViewControllers/ButtonsViewController.swift
@@ -2,8 +2,7 @@
 //  ButtonsViewController.swift
 //  NeumorphicKitExamples
 //
-//  Created by Prashant Shrivastava on 24/10/20.
-//  Copyright © 2020 CRED. All rights reserved.
+//  Copyright 2020 Dreamplug Technologies Private Limited
 //
 
 import UIKit
